5721f6853a741b30b5731d71616b1f131799bd36
Milena Smirnova <milena123323@gmail.com>
2019-05-24 00:11:41 +0300

    webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,17 @@ const path = require('path');
 module.exports = {
   context: path.join(__dirname, 'src'),
   entry: './index.js',
+  devtool: 'source-map',
 
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
   },
 
   resolve: {
