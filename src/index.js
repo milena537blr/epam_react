d3ad91cd22cfcd245bc4c04e78cc8ab87c2ac7e4
milena537blr <milena_smirnova@epam.com>
2019-05-24 18:37:47 +0300

    webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import {someFunction} from './dependency.js';
-
-someFunction();
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 const path = require('path');
+const webpack = require("webpack");
 
 module.exports = {
   context: path.join(__dirname, 'src'),
   entry: './index.js',
   devtool: 'source-map',
+  mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
+  resolve: { extensions: ["*", ".js", ".jsx"] },
 
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
   },
 
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
+  devServer: {
+    contentBase: path.join(__dirname, "dist/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
   },
 
-  resolve: {
-    extensions: ['.js']
-  },
-
-  watch: false
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 }
