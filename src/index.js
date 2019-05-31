8059a6612a44b128cadbd3a2ecb492d5a33cc087
milena537blr <milena_smirnova@epam.com>
2019-05-31 17:49:20 +0300

    eslint errors fixed

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import { App, Component3 } from "./app.js";
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root1')
+  window.document.getElementById('root1')
 )
 
 ReactDOM.render(
-  React.createElement(Component3, {name: 'React.createElement'}, null),
-  document.getElementById('root2')
+  React.createElement(Component3, { name: 'React.createElement' }, null),
+  window.document.getElementById('root2')
 );
 
-// Needed for Hot Module Replacement
-if(typeof(module.hot) !== 'undefined') {
-  module.hot.accept() // eslint-disable-line no-undef  
-}
+// eslint-disable-line no-undef 
