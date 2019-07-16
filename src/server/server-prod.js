3d63977d531c325aac45cc734b1bae17528a6d06
Milena Smirnova <milena123323@gmail.com>
2019-07-16 22:57:01 +0300

    crud-app part 1

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -1,11 +1,13 @@
 import path from 'path';
 import express from 'express';
+import favicon from 'serve-favicon';
 
 const app = express (),
   DIST_DIR = __dirname,
-  HTML_FILE = path.join (DIST_DIR, './dist/index.html');
+  HTML_FILE = path.join (DIST_DIR, 'index.html');
 
 app.use (express.static (DIST_DIR));
+app.use(favicon(path.join('assets','public','favicon.ico')));
 
 app.get ('*', (req, res) => {
   res.sendFile (HTML_FILE);
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,25 +1,29 @@
-const HtmlWebPackPlugin = require ('html-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: 'development',
-  name: 'client',
-  target: 'web',
-  devtool: 'source-map',
+  entry: {
+    main: ["webpack-hot-middleware/client?reload=true"]
+  },
+  mode: "development",
+  name: "client",
+  target: "web",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true,
-              localIdentName: '[local]--[hash:base64:5]',
-            },
+              localIdentName: "[local]--[hash:base64:5]"
+            }
           },
-          'sass-loader',
-        ],
+          "sass-loader"
+        ]
       },
       {
         // Loads the javacript into html template provided.
@@ -27,22 +31,23 @@ module.exports = {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
-          },
-        ],
+            loader: "html-loader"
+          }
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-    ],
+        use: ["file-loader"]
+      }
+    ]
   },
 
   plugins: [
-    new HtmlWebPackPlugin ({
-      template: './src/index.html',
-      filename: './index.html',
-      excludeChunks: ['server'],
-    }),
-  ],
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      excludeChunks: ["server"]
+    })
+  ]
 };
