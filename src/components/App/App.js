5914327b102f3895da7f3ede31bb2f8f47ad95bf
milena537blr <milena_smirnova@epam.com>
2019-07-31 18:03:59 +0300

    undo App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,12 +11,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NotFound from "../NotFound/NotFound";
 import CardsList from "../CardsList/CardsList";
 import { connect } from "react-redux";
-// import * as movieActions from "../../actions/movieActions";
-// import CatList from "../CatList/CatList";
+import * as movieActions from "../../actions/movieActions";
+import CatList from "../CatList/CatList";
 import { loadMovies } from "../../actions/movieActions";
 
 class App extends React.Component {
- /*  constructor(props) {
+  constructor(props) {
     super(props);
     this.cards = this.props.data;
 
@@ -27,13 +27,12 @@ class App extends React.Component {
       filterText: ""
     };
   }
- */
+
   componentDidMount() {
     this.props.dispatch(loadMovies());
-    console.log(this.props);
   }
 
-  /* handleCardClick = currentCardId => {
+  handleCardClick = currentCardId => {
     this.setState({
       isSearchActive: false,
       currentCardId
@@ -48,10 +47,78 @@ class App extends React.Component {
     this.setState({
       isSearchActive: true
     });
-  }; */
+  };
 
   render() {
-    return <div>111</div>
+    const isSearchActive = this.state.isSearchActive;
+    return (
+      <ErrorBoundary>
+        <header className={s.header}>
+          <div className={s.overlay} />
+          <section className={s.container}>
+            {console.log(this.props.movies)}
+            {console.log(this.state)}
+            <Box align="space-between" verticalAlign="middle" marginBottom={8}>
+              <Logo />
+              <Button
+                onHandleSearchClick={this.handleSearchClick}
+                text="SEARCH"
+                areaLabel="Display search area"
+                size="large"
+                color="white"
+                dataTestId="search-switcher"
+              />
+            </Box>
+            <CatList cats={this.props.movies}/>
+            {isSearchActive ? (
+              <Search
+                searchText={this.state.searchText}
+                onSearchTextChange={this.handleSearchTextChange}
+              />
+            ) : (
+              <Article card={this.cards[this.state.currentCardId]} />
+            )}
+          </section>
+        </header>
+        <section className={s.panel}>
+          <Box
+            align="space-between"
+            verticalAlign="middle"
+            className={s.container}
+          >
+            <div>7 movies found</div>
+            <div>
+              <span>Sort by </span>
+              <span>release date </span>
+              <span>rating</span>
+            </div>
+          </Box>
+        </section>
+        <main className={s.main}>
+          <div className={s.container}>
+            <Box className={s.wrapper} align="center">
+              <Route path="/" exact component={NotFound} />
+              <Route
+                path="/search"
+                render={props => (
+                  <CardsList
+                    {...props}
+                    // cards={this.cards}
+                    cards={this.props.movies}
+                    handleCardClick={this.handleCardClick}
+                  />
+                )}
+              />
+            </Box>
+          </div>
+        </main>
+        <footer className={s.footer}>
+          <div className={s.container}>
+            <Logo />
+          </div>
+        </footer>
+      </ErrorBoundary>
+    );
   }
 }
 
