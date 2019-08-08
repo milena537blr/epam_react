e035b62a242e1c1975a8ae3bf7b15d2de0904e9d
milena537blr <milena_smirnova@epam.com>
2019-08-08 15:06:51 +0300

    add test for async actions

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -3,6 +3,7 @@ import * as actions from "./actions";
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import fetchMock from "fetch-mock";
+const FILMS_SOURCE = "http://react-cdp-api.herokuapp.com";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -42,14 +43,42 @@ describe("async actions", () => {
   });
 
   it("creates LOAD_MOVIES_SUCCESS when fetching movies has been done", () => {
-    fetchMock.getOnce("/movies", {
+    fetchMock.getOnce(`${FILMS_SOURCE}/movies`, {
       body: {
+        data: [
+          {
+            budget: 200000,
+            genres: ["Thriller", "Mystery", "Adventure", "Crime", "Romance"],
+            id: 513285,
+            overview:
+              "Private eye Roland Drake cracks cases and romances femme fatales in 1940's Los Angeles while corrupt cop Det Barry Tate rules the city. A tale told in the classic style of film noir. Drake has fallen on hard times in a harsh world. He has been evicted from his office and disgraced by a missing persons case. Ruined in the public eye and with the police. it seems like it's all over for Roland Drake. Then, redemption walks in - with curves. The owner of those curves is a sexy, dark haired beauty named Katherine Montemar. She wants his help. The chemistry is immediate and her concern for the disappearance of her family members pulls him into her case - and into bed.",
+            poster_path:
+              "https://image.tmdb.org/t/p/w500/4dw0z5Uh2NbabdGk2u6wdyJvMg7.jpg",
+            release_date: "2018-04-03",
+            revenue: 0,
+            runtime: 116,
+            tagline:
+              "Passion, Murder, and Betrayal. Just Another Day at the Office.",
+            title: "Trouble Is My Business",
+            vote_average: 0,
+            vote_count: 0
+          }
+        ],
+        limit: 10,
+        offset: 0,
+        total: 3000
+      },
+      headers: { "content-type": "application/json" }
+    });
+
+    const expectedActions = [
+      { type: types.LOAD_MOVIES_LOADING },
+      {
+        type: types.LOAD_MOVIES_SUCCESS,
         movies: [
           {
             budget: 200000,
-            genres: (5)[
-              ("Thriller", "Mystery", "Adventure", "Crime", "Romance")
-            ],
+            genres: ["Thriller", "Mystery", "Adventure", "Crime", "Romance"],
             id: 513285,
             overview:
               "Private eye Roland Drake cracks cases and romances femme fatales in 1940's Los Angeles while corrupt cop Det Barry Tate rules the city. A tale told in the classic style of film noir. Drake has fallen on hard times in a harsh world. He has been evicted from his office and disgraced by a missing persons case. Ruined in the public eye and with the police. it seems like it's all over for Roland Drake. Then, redemption walks in - with curves. The owner of those curves is a sexy, dark haired beauty named Katherine Montemar. She wants his help. The chemistry is immediate and her concern for the disappearance of her family members pulls him into her case - and into bed.",
@@ -65,40 +94,13 @@ describe("async actions", () => {
             vote_count: 0
           }
         ]
-      },
-      headers: { "content-type": "application/json" }
-    });
-
-    const expectedActions = [
-      { type: types.LOAD_MOVIES_LOADING },
-      {
-        type: types.LOAD_MOVIES_SUCCESS,
-        body: {
-          movies: [
-            {
-              budget: 200000,
-              genres: (5)[
-                ("Thriller", "Mystery", "Adventure", "Crime", "Romance")
-              ],
-              id: 513285,
-              overview:
-                "Private eye Roland Drake cracks cases and romances femme fatales in 1940's Los Angeles while corrupt cop Det Barry Tate rules the city. A tale told in the classic style of film noir. Drake has fallen on hard times in a harsh world. He has been evicted from his office and disgraced by a missing persons case. Ruined in the public eye and with the police. it seems like it's all over for Roland Drake. Then, redemption walks in - with curves. The owner of those curves is a sexy, dark haired beauty named Katherine Montemar. She wants his help. The chemistry is immediate and her concern for the disappearance of her family members pulls him into her case - and into bed.",
-              poster_path:
-                "https://image.tmdb.org/t/p/w500/4dw0z5Uh2NbabdGk2u6wdyJvMg7.jpg",
-              release_date: "2018-04-03",
-              revenue: 0,
-              runtime: 116,
-              tagline:
-                "Passion, Murder, and Betrayal. Just Another Day at the Office.",
-              title: "Trouble Is My Business",
-              vote_average: 0,
-              vote_count: 0
-            }
-          ]
-        }
       }
     ];
-    const store = mockStore({ movies: [] });
+    console.log("1111111111111111");
+    console.log(expectedActions);
+    const store = mockStore();
+    console.log(store.dispatch(actions.searchBy));
+    console.log(store.dispatch(actions.loadMovies()));
 
     return store.dispatch(actions.loadMovies()).then(() => {
       // return of async actions
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    console.log(loadMovies());
     this.props.dispatch(loadMovies());
   }
 
