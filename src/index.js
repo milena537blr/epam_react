01e4d21bdcf7121f9d44e79808f4a3f4d14277b1
milena537blr <milena_smirnova@epam.com>
2019-08-20 21:05:42 +0300

    add workbox and service worker

--- src/index.js
+++ src/index.js
@@ -22,8 +22,15 @@ ReactDOM.render(
   window.document.getElementById("root")
 );
 
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/sw.js')
-           .then(function() { console.log("Service Worker Registered"); });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(registration => {
+        console.log("SW registered: ", registration);
+      })
+      .catch(registrationError => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
 }
--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -35,5 +36,14 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+      // directoryIndex: 'index.html'
+    })
+  ]
 };
